635ed781e8fdcb6e5b08b9c973d0c245da17f3ac
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-10 23:35:32 +0300

    Do exercise 1.1

--- Part1/courseinfo/courseinfo/src/index.js
+++ Part1/courseinfo/courseinfo/src/index.js
@@ -10,19 +10,50 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part2={part3} exercises2={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
       <p>
-        {part1} {exercises1}
+       <h1>{props.course}</h1> 
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
       </p>
       <p>
-        {part2} {exercises2}
+        {props.part2} {props.exercises2}
       </p>
       <p>
-        {part3} {exercises3}
+        {props.part3} {props.exercises3}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.total}
+      </p>
     </div>
   )
 }
