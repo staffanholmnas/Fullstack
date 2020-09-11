8f5e027d344d39013ddc029fef873b544b8b76b5
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-09-11 14:18:52 +0300

    Improve exercise 1.2

--- Part1/courseinfo/courseinfo/src/index.js
+++ Part1/courseinfo/courseinfo/src/index.js
@@ -34,9 +34,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part1={props.part1} exercises1={props.exercises1} />
-      <Part part2={props.part2} exercises2={props.exercises2} />
-      <Part part3={props.part3} exercises3={props.exercises3} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -55,13 +55,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
