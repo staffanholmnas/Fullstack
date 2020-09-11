aa2741b3fcfa4c9b4b753d014e9289e8a99c32a8
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-09-11 14:27:28 +0300

    Improve 1.2

--- Part1/courseinfo/courseinfo/src/index.js
+++ Part1/courseinfo/courseinfo/src/index.js
@@ -11,52 +11,52 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} />
       <Content part2={part2} exercises2={exercises2} />
       <Content part2={part3} exercises2={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
-    </div>
+    </>
   )
 }
 
 const Header = (props) => {
   return (
-    <div>
+    <>
         <h1>{props.course}</h1>
-    </div>
+    </>
   )
 }
 
 const Content = (props) => {
  
   return (
-    <div>
+    <>
       <Part part={props.part1} exercises={props.exercises1} />
       <Part part={props.part2} exercises={props.exercises2} />
       <Part part={props.part3} exercises={props.exercises3} />
-    </div>
+    </>
   )
 }
 
 const Total = (props) => {
   return (
-    <div>
+    <>
       <p>
         Number of exercises {props.total}
       </p>
-    </div>
+    </>
   )
 }
 
 const Part = (props) => {
   return (
-    <div>
+    <>
       <p>
         {props.part} {props.exercises}
       </p>
-    </div>
+    </>
   )
 }
 
