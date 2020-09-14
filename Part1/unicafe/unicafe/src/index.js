8736962fd47feb47e069a197b236aa763f5f74f7
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-14 22:01:27 +0300

    Edit exercise 1.6

--- Part1/unicafe/unicafe/src/index.js
+++ Part1/unicafe/unicafe/src/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
