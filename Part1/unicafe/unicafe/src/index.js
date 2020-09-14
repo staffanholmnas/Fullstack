5617074382e8ba7fde051b134cf3776d2612233a
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-14 22:59:36 +0300

    Add exercise 1.7

--- Part1/unicafe/unicafe/src/index.js
+++ Part1/unicafe/unicafe/src/index.js
@@ -10,6 +10,21 @@ const App = () => {
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
 
+  const all = good + neutral + bad
+  const score = good - bad
+
+  const avg = () => {
+    if (all === 0) {
+      return 0
+    } else return score / all
+  }
+  
+  const percentage = () => {
+    if (all === 0) {
+      return 0
+    } else return good / all * 100
+  }
+
   return (
     <>
       <h1>give feedback</h1>
@@ -31,6 +46,9 @@ const App = () => {
       <Display show={"good " + good} />
       <Display show={"neutral " + neutral} />
       <Display show={"bad " + bad} />
+      <Display show={"all " + all} />
+      <Display show={"average " + avg()} />
+      <Display show={"positive " + percentage() + " %"} />
     </>
   )
 }
