5a37a06d4118577bb4940cf717d0ae7236d80969
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-15 12:26:28 +0300

    Do exercise 1.10

--- Part1/unicafe/unicafe/src/index.js
+++ Part1/unicafe/unicafe/src/index.js
@@ -12,28 +12,16 @@ const App = () => {
   const increaseBad = () => setBad(bad + 1)
 
   const statistics = <Statistics good={good} bad={bad} neutral={neutral} />
+  const buttons = <Buttons increaseGood={increaseGood} increaseNeutral={increaseNeutral}
+    increaseBad={increaseBad} />
 
   let combinedFeedback = good + bad + neutral
 
   if (combinedFeedback === 0) {
-
     return (
       <>
         <h1>give feedback</h1>
-        <>
-          <Button
-            handleClick={increaseGood}
-            text="Good"
-          />
-          <Button
-            handleClick={increaseNeutral}
-            text="Neutral"
-          />
-          <Button
-            handleClick={increaseBad}
-            text="Bad"
-          />
-        </>
+        <>{buttons}</>
         <h1>statistics</h1>
         <h3>No feedback given</h3>
       </>
@@ -42,21 +30,19 @@ const App = () => {
   else return (
     <>
       <h1>give feedback</h1>
-      <>
-        <Button
-          handleClick={increaseGood}
-          text="Good"
-        />
-        <Button
-          handleClick={increaseNeutral}
-          text="Neutral"
-        />
-        <Button
-          handleClick={increaseBad}
-          text="Bad"
-        />
-        <>{statistics}</>
-      </>
+      <>{buttons}</>
+      <h1>statistics</h1>
+      <>{statistics}</>
+    </>
+  )
+}
+
+const Buttons = ({ increaseGood, increaseNeutral, increaseBad }) => {
+  return (
+    <>
+      <Button handleClick={increaseGood} text="Good" />
+      <Button handleClick={increaseNeutral} text="Neutral" />
+      <Button handleClick={increaseBad} text="Bad" />
     </>
   )
 }
@@ -65,22 +51,19 @@ const Statistics = ({ good, bad, neutral }) => {
   const all = good + bad + neutral
   return (
     <>
-      <h1>statistics</h1>
-      <Display show={"good " + good} />
-      <Display show={"neutral " + neutral} />
-      <Display show={"bad " + bad} />
-      <Display show={"all " + all} />
-      <Display show={"average " + (good - bad) / all} />
-      <Display show={"positive " + good / all * 100 + " %"} />
+      <Statistic text="good " value={good} />
+      <Statistic text="neutral " value={neutral} />
+      <Statistic text="bad " value={bad} />
+      <Statistic text="all " value={all} />
+      <Statistic text="average " value={(good - bad) / all} />
+      <Statistic text="positive " value={good / all * 100 + " %"} />
     </>
   )
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const Statistic = ({ text, value }) => (<Display show={text + value} />)
+
+const Button = ({ handleClick, text }) => (<button onClick={handleClick}> {text} </button>)
 
 const Display = ({ show }) => <div>{show}</div>
 
