2acac5c0e1320bd8ab03b6bb79617b7372d6997b
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-24 19:14:25 +0300

    Do ex 2.4

--- Part2/courseinfo/src/index.js
+++ Part2/courseinfo/src/index.js
@@ -2,61 +2,85 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
+      <Course courses={courses} />
     </>
   )
 }
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
     <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  )
-}
-
-const Header = ({ course }) => <><h1>{course.name}</h1></>
-
-const Content = ({ course }) => {
-  return (
-    <>
-      {course.parts.map(line =>
-        <Part key={line.id} name={line.name} exercises={line.exercises} id={line.id} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => {
+        return (
+          <div key={course.id}>
+            <Header course={course} />
+            <Content course={course} />
+            <Total course={course} />
+          </div>
+        )
+      }
       )}
     </>
   )
 }
+
+const Header = ({ course }) => <><h2>{course.name}</h2></>
+
+const Content = ({ course }) =>
+  <>
+    {course.parts.map(line =>
+      <Part key={line.id} name={line.name} exercises={line.exercises}
+      />
+    )}
+  </>
 
 const Total = ({ course }) => {
   let sum = course.parts.reduce((total, currentValue) => {
