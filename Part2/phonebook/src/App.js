c7089a2435c88cab338abab28510f8bfc18cec4e
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-24 23:29:20 +0300

    Do exercise 2.8

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1234567' }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
         const personObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         }
 
         if (persons.map(name =>
@@ -19,7 +21,9 @@ const App = () => {
         }
         else {
         setPersons(persons.concat(personObject))
-        setNewName("")}
+        setNewName("")
+        setNewNumber("")
+      }
     }
 
     const errorMessage = () => {
@@ -29,6 +33,10 @@ const App = () => {
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+      setNewNumber(event.target.value)
     }
 
     return (
@@ -42,13 +50,19 @@ const App = () => {
                     />
                 </div>
                 <div>
+                    number: <input 
+                        value={newNumber}
+                        onChange={handleNumberChange}
+                    />
+                  </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             {persons.map(person =>
                 <div key={person.name}>
-                    {person.name}
+                    {person.name} {person.number}
                 </div>
             )}
         </div>
