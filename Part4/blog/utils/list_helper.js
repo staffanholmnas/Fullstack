2dc202c1e989cc82f933e8dbf4d74e3205e8f654
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-17 18:23:57 +0300

    Do ex 4.4

--- Part4/blog/utils/list_helper.js
+++ Part4/blog/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = (blogs) => {
+    return (
+        blogs
+            .map(blog => blog.likes)
+            .reduce((a, b) => a + b, 0)
+    )
+}
+
+module.exports = {
+    dummy, totalLikes
+}
