d300216118a269f9fdbac036bf8fbb1d775aff48
staffanholmnas <staffan.holmnas@centria.fi>
2020-11-06 13:13:13 +0200

    Do ex 5.18

--- Part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ Part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,36 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Log in to application')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged-in')
+      cy.contains('logout')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+
+      cy.get('.error').contains('wrong username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
