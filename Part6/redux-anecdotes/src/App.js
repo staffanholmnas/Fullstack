a52724a12cd92f17c75fc2576911090ad315797f
staffanholmnas <staffan.holmnas@centria.fi>
2020-11-08 19:49:24 +0200

    Do ex 6.5

--- Part6/redux-anecdotes/src/App.js
+++ Part6/redux-anecdotes/src/App.js
@@ -22,10 +22,14 @@ const App = () => {
     })
   }
 
+  const sortedAnecdotes = (anecdotes) =>{
+    return anecdotes.sort((a, b) => b.votes - a.votes)
+  }
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes(anecdotes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
