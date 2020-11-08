fb50574f9f8ea90de67c9d6c8b1f826291c735b1
staffanholmnas <staffan.holmnas@centria.fi>
2020-11-08 22:37:11 +0200

    Do ex 6.8

--- Part6/redux-anecdotes/src/App.js
+++ Part6/redux-anecdotes/src/App.js
@@ -1,34 +1,12 @@
 import React from 'react'
-import AnecdoteForm from './components/anecdoteForm'
-import { useSelector, useDispatch } from 'react-redux'
-import { giveVote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(giveVote(id))
-  }
-
-  const sortedAnecdotes = (anecdotes) => {
-    return anecdotes.sort((a, b) => b.votes - a.votes)
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes(anecdotes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -21,8 +21,8 @@ const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
 
-  console.log('state now: ', state)
-  console.log('action', action)
+  //console.log('state now: ', state)
+  //console.log('action', action)
 
   switch (action.type) {
     case 'VOTE': {
