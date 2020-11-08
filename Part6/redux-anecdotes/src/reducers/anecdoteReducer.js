87dc6692455034aadfeafe75844e429db75412cc
staffanholmnas <staffan.holmnas@centria.fi>
2020-11-08 19:24:00 +0200

    Do ex 6.3

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,23 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
+
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.data.id
+      const anecdoteToChange = state.find(a => a.id === id)
+      const changedAnecdote = { ...anecdoteToChange }
+      changedAnecdote.votes++
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+    }
+    default:
+      return state
+  }
 }
 
 export default reducer
