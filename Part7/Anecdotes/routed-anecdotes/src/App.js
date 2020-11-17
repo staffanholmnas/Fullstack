161d18781d5deaed9086fde5288f58300318b8ae
staffanholmnas <staffan.holmnas@centria.fi>
2020-11-17 16:18:41 +0200

    Do ex 7.4

--- Part7/Anecdotes/routed-anecdotes/src/App.js
+++ Part7/Anecdotes/routed-anecdotes/src/App.js
@@ -7,6 +7,7 @@ import {
   useParams,
   useHistory
 } from "react-router-dom"
+import { useField } from './hooks'
 
 
 const Menu = () => {
@@ -72,9 +73,9 @@ const Footer = () => (
 let timeoutID
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const history = useHistory()
   
@@ -83,10 +84,10 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
-      votes: 0
+      content : content.value,
+      author : author.value,
+      info : info.value,
+      votes : 0
     })
     history.push('/')
   }
@@ -97,15 +98,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} name='content' />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} name='author' />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} name='info' />
         </div>
         <button>create</button>
       </form>
