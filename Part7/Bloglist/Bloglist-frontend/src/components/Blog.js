225e239d396c8c0abf28b06643523f370780c7e3
staffanholmnas <staffan.holmnas@centria.fi>
2020-11-26 11:45:29 +0200

    Do ex 7.18

--- Part7/Bloglist/Bloglist-frontend/src/components/Blog.js
+++ Part7/Bloglist/Bloglist-frontend/src/components/Blog.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import commentService from '../services/comments'
+import { initializeBlogs } from '../reducers/blogReducer'
 
 const Blog = ({ handleLike, handleRemove, owner }) => {
+  const [comment, setComment] = useState('')
+  const dispatch = useDispatch()
 
   const blogs = useSelector(state => state.blogs)
   const id = useParams().id
 
   const blog = blogs.find(blog => blog.id === id)
 
+  useEffect(() => {
+    dispatch(initializeBlogs())
+  }, [dispatch, blog])
+
   if (!blog) return null
 
   const isOwner = owner === blog.user.username
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+  
+    const newComment = {
+      comments: comment
+    }
+
+    try {
+      await commentService.create(newComment, id)
+    }
+    catch (exception) {
+      console.log(exception)
+    }
+  
+    setComment('')
+  }
 
   return (
     <div>
@@ -22,8 +47,15 @@ const Blog = ({ handleLike, handleRemove, owner }) => {
       <div>{isOwner && <button onClick={() => handleRemove(blog.id)}>remove</button>}</div>
       <p></p>
       <h4>Comments</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={comment}
+          onChange={({ target }) => setComment(target.value)}
+        />
+        <button type="submit">Add comment</button>
+      </form>
       <ul>
-        {blog.comments.map(blog => <li key={blog}> {blog}</li>)}
+        {blog.comments.map((blog, index) => <li key={index}> {blog}</li>)}
       </ul>
     </div>
   )
